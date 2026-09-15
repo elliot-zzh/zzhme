af5519bfc33e644601a20d17c019e03ea54d503e
Elliot Zhang <elliot_zh@proton.me>
2026-09-15 21:39:45 +0800

    chore: remove netlify adapter for pure static output

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
 import vue from '@astrojs/vue'
 import mdx from '@astrojs/mdx'
-import netlify from '@astrojs/netlify'
 
 export default defineConfig({
   site: 'https://zzhme.netlify.app',
@@ -11,12 +10,5 @@ export default defineConfig({
     vue(),
     mdx()
   ],
-  output: 'static',
-  adapter: netlify({
-    devFeatures: {
-      edgeFunctions: false,
-      environmentVariables: true,
-      images: true,
-    },
-  })
+  output: 'static'
 })
